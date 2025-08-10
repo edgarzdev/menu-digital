2b9d693a6d138578571227ad8ca368c70af69d75
marco-lopez-uab <marco.lopez@uab.edu.bo>
2025-08-10 18:38:52 -0400

    Enlazar archivo CSS externo y actualizar estilos de mensajes de notificación - Se añadió el enlace al archivo CSS externo en el HTML para separar estilos. - Se modificaron y mejoraron los estilos de las clases  y . - Ahora los mensajes de error y éxito tienen un diseño más moderno y responsivo. - Se implementó estilo fijo para mostrar notificaciones en la parte superior de la pantalla.

--- app/views/auth.php
+++ app/views/auth.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Login | Menu Digital</title>
+    <link rel="stylesheet" href="<?= URL ?>/css/style.css">
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
 
