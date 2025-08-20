2ed5d91a6338f4d011e4dd4c1fe68e69fc9f7304
edgarzdev <01edgar.rz@gmail.com>
2025-08-19 20:08:37 -0400

    crear migraciones de categorias y productos para la base de datos

---
+++ backend/database/migrations/2025_08_19_035750_create_productos_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+ Schema::create('producto', function (Blueprint $table) {
+            $table->id('id_producto');
+            $table->string('nombre', 100);
+            $table->text('descripcion')->nullable();
+            $table->decimal('precio', 10, 2);
+            $table->string('imagen', 255)->nullable();
+            $table->unsignedBigInteger('categoria_id');
+            $table->boolean('estado')->default(1);
+
+            $table->foreign('categoria_id')
+                  ->references('id_categoria')
+                  ->on('categoria')
+                  ->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('productos');
+    }
+};
